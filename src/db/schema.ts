e698e3ec06ff4bf28acd1c7b383e1a08f58df6c6
Saurav Jha <srvtj629@gmail.com>
2026-03-29 21:37:07 +0530

    drizzle setup

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  text,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  firstName: varchar("first_name", { length: 45 }).notNull(),
+  lastName: varchar("last_name", { length: 45 }),
+  email: varchar("email", { length: 322 }).notNull().unique(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+  password: varchar("password", { length: 66 }),
+  salt: text("salt"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  //   updatedAt: timestamp().$onUpdatenotNull(),
+});
